e45d5d35c837109aa1b70e44c13aec7a0ffca8bf
skylardann <skylardann@cca.edu>
2017-02-26 23:26:03 -0800

    Create Lewitt-381.js

---
+++ Lewitt-381.js
@@ -0,0 +1,30 @@
+function setup() {
+
+  createCanvas(700, 650);
+  background(0);
+}
+
+function draw() {
+  
+  noStroke(); 
+    
+
+  fill(200, 196, 193);
+
+  rectMode(RADIUS);
+
+  rect(200, 100, 250, 200);
+
+  fill(253, 253, 98);
+
+  rect(600, 100, 250, 200);
+
+  fill('#ec4c7a');
+
+  rect(200, 500, 250, 200);
+
+  fill(0, 164, 217);
+  
+  rect(600, 500, 250, 200);
+
+}
