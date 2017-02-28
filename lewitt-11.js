b451b2ac573692fe81ddc94628652860a4be8416
skylardann <skylardann@cca.edu>
2017-02-27 21:21:33 -0800

    Update lewitt-11.js

--- lewitt-11.js
+++ lewitt-11.js
@@ -3,10 +3,15 @@ function setup() {
   createCanvas(405, 405);
   background(206, 207, 209, 33);
 
+
+  frameRate(5);
+
   for (var z = 0; z <= 400; z += 10) {
-    stroke(170, 171, 173);
+    stroke(180, 171, 173);
 
     line(0, z, 400, z)
+    stroke(180, 171, 173);
+
     line(z, 0, z, 400)
 
   }
@@ -14,15 +19,21 @@ function setup() {
 var b = 10;
 
 function draw() {
+  
+  
 
   strokeWeight(0.25);
 
-  stroke(170, 171, 173);
+  stroke(140, 171, 173);
 
   if (b <= 200) {
+
     line(b, 0, 0, b);
+    stroke(140, 171, 173);
     line(200, b, b, 200);
+    stroke(140, 171, 173);
     line(200, 200 - b, 200 + b, 200);
+    stroke(140, 171, 173);
     line(200 + b, 0, 400, 200 - b);
     line(200, 200 + b, 200 - b, 200);
     line(200 - b, 400, 0, 200 + b);
@@ -30,4 +41,7 @@ function draw() {
     line(200 + b, 400, 400, 200 + b);
     b += 10;
   }
+  
 }
+
+
