bbaa071d24ebf9c68f90d790315c6bf475537a90
skylardann <skylardann@cca.edu>
2017-03-01 08:57:40 -0800

    Update arrays-1.js

--- arrays-1.js
+++ arrays-1.js
@@ -1,20 +1,26 @@
-
 var shade = [10, 20, 45, 25, 60];
 
 
 function setup() {
   createCanvas(400, 400);
-  
 }
 
 function draw() {
   colorMode(HSB);
   noStroke();
 
-  for (var i = 0; i < 5; i += 1) {
+  fill(shade[0], 100, 100);
+  rect(0*width/5, 0, width/5, height);
 
-    fill(shade[i], 100, 100);
-    rect(0 + i * width / 9, 0, width / 9, height);
+  fill(shade[1], 100, 100);
+  rect(1*width/5, 0, width/5, height);
 
-  }
+  fill(shade[2], 100, 100);
+  rect(2*width/5, 0, width/5, height);
+
+  fill(shade[3], 100, 100);
+  rect(3*width/5, 0, width/5, height);
+
+  fill(shade[4], 100, 100);
+  rect(4*width/5, 0, width/5, height);
 }
